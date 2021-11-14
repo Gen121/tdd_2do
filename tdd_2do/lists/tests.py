1761dd597bb66ff7d49c31a1a99bb22cc92188ad
Евгений Челноков <grem1in.che@yandex.ru>
2021-11-14 20:40:37 +0300

    0.0.6 Added first modul test for views home_page

--- tdd_2do/lists/tests.py
+++ tdd_2do/lists/tests.py
@@ -1,8 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
 
-class SmokeTest(TestCase):
-    """Тест на токсичность"""
-    def test_bad_maths(self):
-        """Тест неправильные математические расчеты"""
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    """Теcт домашней страницы"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """Тест корневой url преобразуется в представление
+        домашней страницы
+        """
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
