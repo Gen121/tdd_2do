59b738c01d71ddb35832f70a6ceeee2e481f6c83
Евгений Челноков <grem1in.che@yandex.ru>
2021-11-14 18:39:12 +0300

    0.0.3 Fixed error < attempted relative import beyond top-level package>,  added comments functional_tests.py

--- tdd_2do/tdd_2do/settings.py
+++ tdd_2do/tdd_2do/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from env._secret import s_key
+from ._secret import s_key
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
