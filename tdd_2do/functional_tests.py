86cb950f10d067cce7aee963aebcdc79475b92c9
Eugene Che <grem1in.che@yandex.ru>
2022-04-06 18:48:46 +0300

    Functional test - added ability to enter a list item Sorted imports

--- tdd_2do/functional_tests.py
+++ tdd_2do/functional_tests.py
@@ -1,6 +1,8 @@
+import time
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -23,17 +25,34 @@ class NewVisitorTest(unittest.TestCase):
 
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
-        self.assertIs('To-Do', self.browser.title)
-        self.fail('Закончить текст!')
+        self.assertIn('To-Do lists', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
         # вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
+
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(0.5)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Купить павлиньи перья' for row in rows)
+        )
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        self.fail('Закончить текст!')
         # Страница снова обновляется, и теперь показывает оба элемента ее
         # списка Эдит интересно, запомнит ли сайт ее список. Далее она видит,
         # что сайт сгенерировал для нее уникальный URL-адрес – об этом
--- tdd_2do/lists/tests.py
+++ tdd_2do/lists/tests.py
@@ -1,6 +1,7 @@
-from django.urls import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import resolve
+
 from lists.views import home_page
 
 
