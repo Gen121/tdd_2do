dbb2ed1bf943ee3b47b5e08b2dcbdb96bc13aaa2
Eugene Che <grem1in.che@yandex.ru>
2022-04-08 13:19:30 +0300

    View home_page create Item obj

--- tdd_2do/lists/views.py
+++ tdd_2do/lists/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from lists.models import Item
 
 
 def home_page(request):
     """домашняя страница"""
     context = {'new_item_text': request.POST.get('item_text', '')}
+    item = Item()
+    item.text = context['new_item_text']
+    item.save()
+    context['new_item_text'] = item.text
     return render(request, 'home.html', context=context)
