b93cee854fe3c232350437356ec8e95c42563afb
Eugene Che <grem1in.che@yandex.ru>
2022-04-08 12:05:38 +0300

    одель для элементов списка и связанная с ней миграция

--- tdd_2do/lists/tests.py
+++ tdd_2do/lists/tests.py
@@ -1,29 +1,50 @@
-from django.http import HttpRequest
+from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import resolve
 
-from lists.views import home_page
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
     """Теcт домашней страницы"""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Тест корневой url преобразуется в представление домашней страницы
-        """
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_use_correct_template(self):
         """тест: домашняя страница возвращает правильный html
         """
-        response = self.client.get('/')
+        response: HttpResponse = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
         """тест: можно сохранить POST запрос
         """
-        response = self.client.post('/', data={
-            'item_text': 'A new list item'})
+        response: HttpResponse = self.client.post(
+            '/', data={'item_text': 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTesr(TestCase):
+    """Тест модели элемента списка
+    """
+
+    def test_saving_and_retriving_items(self):
+        # Зто - Интегрированный тест тк опирается на внешнюю систему – бд
+        # Этот модульный тест в очень многословном стиле
+        # Не рекомендуется писать так в реальной ситуации
+        # В главе 15 мы этот тест перепишем, чтобы он стал намного короче
+        """тест: сохранение и получение моделей списка"""
+        first_item = Item()
+        first_item.text = 'The first (Ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (Ever) list item')
+        self.assertEqual(second_saved_item.text, 'Item the second')
