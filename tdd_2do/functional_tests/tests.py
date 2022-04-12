3dbf760c865c1a993197ec8da42fe2d3dbf3c976
Eugene Che <grem1in.che@yandex.ru>
2022-04-12 17:45:57 +0300

    Added functional_test for aligin inputbox by center

--- tdd_2do/functional_tests/tests.py
+++ tdd_2do/functional_tests/tests.py
@@ -128,6 +128,27 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Сделать мушку из павлиньих перьев', page_text)
         self.assertIn('Купить молоко', page_text)
 
+    def test_layout_and_styling(self):
+        '''тест макета и стилевого оформления'''
+        # Эдит открывает домашнюю страницу
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        # Она замечает, что поле ввода аккуратно центрировано
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=11)
+
+        # Она начинает новый список и видит, что поле ввода там тоже
+        # аккуратно центировано
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=11)
+
         self.fail('Закончить тест!')
-        # Она посещает этот URL-адрес – ее список по-прежнему там.
-        # Удовлетворенная, она снова ложится спать
