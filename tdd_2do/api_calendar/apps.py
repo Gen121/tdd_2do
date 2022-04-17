92f34a83db4d3158b2b60a9a13bb240e0fe08a9d
Eugene Che <grem1in.che@yandex.ru>
2022-04-17 13:37:34 +0300

    Create api_calendar

---
+++ tdd_2do/api_calendar/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ApiCalendarConfig(AppConfig):
+    name = 'api_calendar'
--- tdd_2do/tdd_2do/settings.py
+++ tdd_2do/tdd_2do/settings.py
@@ -27,7 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lists.apps.ListsConfig'
+    'lists.apps.ListsConfig',
+    'api_calendar.apps.ApiCalendarConfig',
 ]
 
 MIDDLEWARE = [
